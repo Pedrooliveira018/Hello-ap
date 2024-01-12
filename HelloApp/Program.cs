7bccf5060c22df7db0bcb5a8c641f2d41c1c1de4
Pedro Oliveira <pedro.ata1240@gmail.com>
2024-01-11 21:54:03 -0300

    Primeiro commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,22 @@
+﻿//Imprime um texto na tela.
+Console.WriteLine("Hello World!");
+
+Console.WriteLine("Seja bem vindo ao meu primeiro programa!");
+
+string nome = "Pedro Oliveira";
+Console.WriteLine(nome + " é meu nome!");
+
+// Criação de duas variaveis.
+string nome;
+int Idade;
+
+Console.WriteLine("Qual é seu nome?");
+
+// Lê o mome digitado pelo usuário e armazena na variável nome.
+nome = Console.ReadLine();
+Console.WriteLine("Prazer em conhecer" + nome + "!");
+
+Console.WriteLine("Qual é a sua idade?");
+Idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Você tem " + idade + " anos de idade.")
